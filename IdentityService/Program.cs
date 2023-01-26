f7be792af13f1cd2332a1001cdc0683e09819f5e
Stephan <57194608+stephan418@users.noreply.github.com>
2023-01-26 15:35:49 +0100

    Initialize server project (IdentityServer)
    + Add Visual Studio Solution for the whole Hubertyne project
    + Add sample code and install packages for OpenIddict

--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -1,6 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// OpenIddict
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseInMemoryDatabase(nameof(DbContext));
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore().UseDbContext<DbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetTokenEndpointUris("connect/token");
+        options.AllowClientCredentialsFlow();
+
+        options.AddDevelopmentEncryptionCertificate();
+        options.AddDevelopmentSigningCertificate();
+
+        options.UseAspNetCore()
+            .EnableAuthorizationEndpointPassthrough();
+    })
+    .AddValidation(options =>
+    {
+        options.UseLocalServer();
+
+        options.UseAspNetCore();
+    });
+
+// builder.Services.AddHostedService<Worker>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+app.UseRouting();
 
 app.Run();
