2a3c2e537f1291b1e023a2205aaaab54a77d8ed2
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-02-02 15:15:57 +0100

    Feature/login (#29)
    * added component library
    
    * added basic front-end login functionality
    
    ---------
    
    Co-authored-by: Laurin <60652077+Flexla54@users.noreply.github.com>

--- ClientSpa/src/app/app.module.ts
+++ ClientSpa/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { ButtonModule } from 'primeng/button'
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
