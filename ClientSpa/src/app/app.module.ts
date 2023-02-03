10e7830cccd048f6974c74443e94b0297970e98c
La_Felx <60652077+Flexla54@users.noreply.github.com>
2023-02-03 01:37:32 +0100

    added html, primeng icons+themes

--- ClientSpa/src/app/app.module.ts
+++ ClientSpa/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { PasswordModule } from "primeng/password";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { SignupComponent } from "./signup/signup.component";
 import { InputTextModule } from "primeng/inputtext";
-
 import { DividerModule } from "primeng/divider";
 import { InputNumberModule } from "primeng/inputnumber";
+
 import { ApiService } from "./shared/services/api.service";
 
 @NgModule({
