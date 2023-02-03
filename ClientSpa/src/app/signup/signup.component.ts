971d83705ea14223a753e8c44638a9a8076bdd97
La_Felx <60652077+Flexla54@users.noreply.github.com>
2023-02-03 01:37:32 +0100

    added html, primeng icons+themes

--- ClientSpa/src/app/signup/signup.component.ts
+++ ClientSpa/src/app/signup/signup.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent {
-
+  username: string = "";
+  email: string = "";
+  pw1: string = "";
+  pw2: string = "";
+  maxconsumption: number = 0;
 }
