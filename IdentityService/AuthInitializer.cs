86ba8598f2964a495d1044f7196b928d1fb3372c
Stephan <57194608+stephan418@users.noreply.github.com>
2023-02-05 20:06:24 +0100

    Add barebones identity server

---
+++ IdentityService/AuthInitializer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenIddict.Abstractions;
+
+namespace IdentityService
+{
+    // Service class to initialize the OpenIddict applications
+    public class AuthInitializer : IHostedService
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public AuthInitializer(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+            await context.Database.EnsureCreatedAsync(cancellationToken);
+
+            var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+
+            // Debug application
+            if (await manager.FindByClientIdAsync("debug", cancellationToken) is null)
+            {
+                await manager.CreateAsync(new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "debug",
+                    DisplayName = "Debug application (removed in production)",
+                    RedirectUris = { new Uri("about:blank"), new Uri("https://oauthdebugger.com/debug") },
+                    Type = OpenIddictConstants.ClientTypes.Public,
+                    Permissions =
+                    {
+                        OpenIddictConstants.Permissions.Endpoints.Token,
+                        OpenIddictConstants.Permissions.Endpoints.Authorization,
+                        OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+                        OpenIddictConstants.Permissions.Prefixes.Scope + "api",
+                        OpenIddictConstants.Permissions.ResponseTypes.Code
+                    }
+                }, cancellationToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
