946bcdbe838534217bc986f516fd8b7446954d29
Stephan <57194608+stephan418@users.noreply.github.com>
2023-02-05 20:06:24 +0100

    Add barebones identity server

---
+++ IdentityService/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using IdentityService.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IdentityService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Debug: Do not allow login when the password is "123"
+            if (loginDto.Password == "123")
+            {
+                return Unauthorized();
+            }
+
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, loginDto.Username) };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+            return NoContent();
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync();
+
+            return NoContent();
+        }
+    }
+}
--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -1,12 +1,35 @@
+using IdentityService;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// OpenIddict
+// Setup cookie authentication and redirect to the /login page when challenged to authenticate
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        // TODO: Change redirection to production site
+        options.LoginPath = "/login";
+    });
+
+// Add allow-all CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("*", builder =>
+    {
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+        builder.AllowAnyOrigin();
+    });
+});
+
+// Add service which creates test applications
+builder.Services.AddHostedService<AuthInitializer>();
+
+// DbContext for OpenIddict
 builder.Services.AddDbContext<DbContext>(options =>
 {
     options.UseInMemoryDatabase(nameof(DbContext));
@@ -20,13 +43,22 @@ builder.Services.AddOpenIddict()
     })
     .AddServer(options =>
     {
-        options.SetTokenEndpointUris("connect/token");
-        options.AllowClientCredentialsFlow();
 
+        // Enable needed endpoints and authorization code flow + PKCE
+        options.SetTokenEndpointUris("connect/token");
+        options.SetAuthorizationEndpointUris("connect/authorize");
+        options.AllowAuthorizationCodeFlow().RequireProofKeyForCodeExchange();
+
+        // TODO: Generate "real" certificates when in production
         options.AddDevelopmentEncryptionCertificate();
         options.AddDevelopmentSigningCertificate();
+        options.DisableAccessTokenEncryption(); // TODO: Enable encryption in production
+
+        // We do only support the global "api" scope
+        options.RegisterScopes("api");
 
         options.UseAspNetCore()
+            .EnableTokenEndpointPassthrough()
             .EnableAuthorizationEndpointPassthrough();
     })
     .AddValidation(options =>
@@ -36,19 +68,25 @@ builder.Services.AddOpenIddict()
         options.UseAspNetCore();
     });
 
-// builder.Services.AddHostedService<Worker>();
-
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.MapControllers();
 app.UseRouting();
 
+app.UseCors();
+
+app.UseAuthentication();
+
+app.UseEndpoints((_) => { });
+
+// Proxy the AuthSpa so that it can be used form the same origin as the API
+// (because of cookie policies)
+app.UseSpa(x =>
+{
+    x.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
+});
+
 app.Run();
+
