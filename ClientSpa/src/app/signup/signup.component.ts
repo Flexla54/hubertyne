194cb85c2a3c32c4ee51f165fe27ab334b7a731d
Laurin <60652077+Flexla54@users.noreply.github.com>
2023-02-12 03:44:53 +0100

    added form validation logic

--- ClientSpa/src/app/signup/signup.component.ts
+++ ClientSpa/src/app/signup/signup.component.ts
@@ -1,33 +1,78 @@
-import { Component } from "@angular/core";
-import { ApiService } from "../shared/services/api.service";
-import { DelegationService } from "../shared/services/delegation.service";
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../shared/services/api.service';
+import { DelegationService } from '../shared/services/delegation.service';
+import { PasswordMatchValidator } from '../shared/password-match.directive';
 
 @Component({
-  selector: "app-signup",
-  templateUrl: "./signup.component.html",
-  styleUrls: ["./signup.component.scss"],
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
-  username: string = "";
-  email: string = "";
-  pw1: string = "";
-  pw2: string = "";
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  signupStatus: {
+    isEmpty: boolean;
+    invEmail: boolean;
+    difPw: boolean;
+  } = {
+    isEmpty: false,
+    invEmail: false,
+    difPw: false,
+  };
 
   constructor(
     private readonly api: ApiService,
-    private readonly delegation: DelegationService
+    private readonly delegation: DelegationService,
+    private formBuilder: FormBuilder
   ) {}
 
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        pw1: ['', Validators.required],
+        pw2: ['', Validators.required],
+      },
+      { validators: PasswordMatchValidator }
+    );
+
+    this.signupForm.valueChanges.subscribe((value) => {
+      this.signupStatus.isEmpty =
+        (this.f['username'].errors?.['required'] ||
+          this.f['email'].errors?.['required'] ||
+          this.f['pw1'].errors?.['required'] ||
+          this.f['pw2'].errors?.['required']) &&
+        !this.f['username'].pristine &&
+        !this.f['email'].pristine &&
+        !this.f['pw1'].pristine &&
+        !this.f['pw2'].pristine;
+
+      this.signupStatus.invEmail = this.f['email'].errors?.['email'];
+      this.signupStatus.difPw = this.signupForm.errors?.['passwordDiffers'];
+    });
+  }
+
+  get f() {
+    return this.signupForm.controls;
+  }
+
   public onSubmit() {
-    if (this.pw1 === this.pw2 && this.username == "" && this.email == "") {
-      this.api
-        .signup(this.username, this.email, this.pw1)
-        .catch((e) => {
-          // error handling code
-        })
-        .then(() => {
-          this.delegation.redirect();
-        });
-    }
+    this.api
+      .signup(
+        this.f['username'].value,
+        this.f['email'].value,
+        this.f['pw1'].value
+      )
+      .catch((e) => {
+        // error handling code
+
+        this.f['pw1'].setValue('');
+        this.f['pw2'].setValue('');
+      })
+      .then(() => {
+        this.delegation.redirect();
+      });
   }
 }
