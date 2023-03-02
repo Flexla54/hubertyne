f81250c0292293282d67b2c12920d5d5a86539e5
Florian <58337160+OmengU@users.noreply.github.com>
2023-03-02 16:47:10 +0100

    Added Model for Plug, Added Repository for Plug (not finished)

---
+++ PlugService/Models/Plug.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlugService.Models
+{
+    public class Plug
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsConnected { get; set; }
+        public bool IsTurnedOn { get; set; }
+        public Guid UserId { get; set; }
+
+    }
+}
