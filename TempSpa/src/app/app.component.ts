fb2415ef65eec8460b3c17bae400059197e4ebbe
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-03-02 15:18:45 +0100

    new project for main page, currently empty dashboard (#33)

---
+++ TempSpa/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './auth.config';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TempSpa';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+}
---
+++ TempSpa/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  display: boolean = false;
+  plugdata: any;
+
+  ngOnInit() {
+    let week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    for (let i = 0; i < (new Date).getDay() + 6; i++) {
+      week.push(week.shift()!);
+    }
+
+    let data = [];
+    let avg_data = []
+    for (let i = 0; i < 7; i++) {
+      data.push(Math.floor(Math.random() * 500));
+      avg_data.push(Math.floor(Math.random() * 125) + Math.floor(Math.random() * 125) + Math.floor(Math.random() * 125) + Math.floor(Math.random() * 125));
+    }
+
+    this.plugdata = {
+      labels: week,
+      datasets: [
+        {
+            label: 'Plug Consumption last week',
+            data: data,
+            fill: false,
+            borderColor: '#42A5F5',
+            tension: 0
+        },
+        {
+            label: 'avg. last 5 weeks',
+            data: avg_data,
+            fill: false,
+            borderColor: '#A542F5',
+            tension: 0.5
+        }
+      ]
+  };
+  }
+}
