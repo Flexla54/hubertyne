ab161aa85833002d15689cd24785211c43324467
Florian <58337160+OmengU@users.noreply.github.com>
2023-03-02 16:47:10 +0100

    Added Model for Plug, Added Repository for Plug (not finished)

---
+++ PlugService/Models/IPlugRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace PlugService.Models
+{
+    public interface IPlugRepository
+    {
+        IEnumerable<Plug> All { get; }
+        Plug CreatePlug(string name, Guid user);
+        Plug GetbyId(Guid id);
+        List<Plug> GetbyUserId(Guid id);
+        Plug ChangeName(Guid id, string name);
+        void AddPlug(Plug plug);
+
+        Plug ChangePowerStatus(Guid id, bool status);
+        Plug ChangeConnectionStatus(Guid id, bool status);
+
+    }
+}
