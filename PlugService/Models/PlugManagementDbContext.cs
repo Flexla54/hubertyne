014cc206fa99fb2c340612179273d3dd902b2fac
Florian <58337160+OmengU@users.noreply.github.com>
2023-03-02 16:47:10 +0100

    Added Model for Plug, Added Repository for Plug (not finished)

---
+++ PlugService/Models/PlugManagementDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFramework.Exceptions.PostgreSQL;
+namespace PlugService.Models
+{
+    public class PlugManagementDbContext : DbContext
+    {
+        public PlugManagementDbContext(DbContextOptions<PlugManagementDbContext> options) : base(options) { }
+        public DbSet<Plug> Plugs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseExceptionProcessor();
+        }
+    }
+}
---
+++ PlugService/Models/PlugRepository.cs
@@ -0,0 +1,55 @@
+﻿namespace PlugService.Models
+{
+    public class PlugRepository : IPlugRepository
+    {
+        private readonly PlugManagementDbContext _context;
+        public PlugRepository(PlugManagementDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Plug> All => _context.Plugs;
+
+        public void AddPlug(Plug plug)
+        {
+            _context.Plugs.Add(plug);
+        }
+        public Plug CreatePlug(string name, Guid user)
+        {
+            Plug plug = new Plug()
+            {
+                Name = name,
+                UserId = user,
+                IsConnected = false,
+                IsTurnedOn = false,
+            };
+            AddPlug(plug);
+            _context.SaveChanges();
+            return plug;
+        }
+
+        public Plug ChangeConnectionStatus(Guid id, bool status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Plug ChangeName(Guid id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Plug ChangePowerStatus(Guid id, bool status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Plug GetbyId(Guid id)
+        {
+            return _context.Plugs.Where(_ => _.Id == id).First();
+        }
+
+        public List<Plug> GetbyUserId(Guid id)
+        {
+            return _context.Plugs.Where(_ => _.UserId == id).ToList();
+        }
+    }
+}
