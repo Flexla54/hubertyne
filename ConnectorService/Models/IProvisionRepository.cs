9c460d9de327216f273f46e83e7628a5b5620863
Stephan <57194608+stephan418@users.noreply.github.com>
2023-03-05 00:08:17 +0100

    Add connector service

---
+++ ConnectorService/Models/IProvisionRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ConnectorService.Models
+{
+    public interface IProvisionRepository
+    {
+        IEnumerable<Provision> All { get; }
+
+        Provision CreateProvision(Guid userId, Model model, string? description);
+
+        void DeleteProvision(Guid id);
+
+        Provision? GetById(Guid id);
+
+        IEnumerable<Provision> GetByUserId(Guid userId);
+    }
+}
