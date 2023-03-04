0bed5a3fb5080043e2470a422a448edb07899b84
Stephan <57194608+stephan418@users.noreply.github.com>
2023-03-05 00:08:17 +0100

    Add connector service

---
+++ ConnectorService/Controllers/ProvisionsController.cs
@@ -0,0 +1,54 @@
+﻿using ConnectorService.Dtos;
+using ConnectorService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvisionsController : ControllerBase
+    {
+        private readonly ProvisionRepository _provisionRepository;
+
+        public ProvisionsController(ProvisionRepository provisionRepository)
+        {
+            _provisionRepository = provisionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProvisions([FromQuery] Guid? userId)
+        {
+            if (userId is not null)
+            {
+                return Ok(_provisionRepository.GetByUserId((Guid) userId));
+            }
+
+            return Ok(_provisionRepository.All);
+        }
+
+        [HttpPost]
+        public IActionResult AddProvision([FromBody] AddProvisionDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Provision provision = _provisionRepository.CreateProvision(
+                Guid.Parse("0d7dd136-5e82-42f2-bc5c-bb5bfdd13639"),
+                dto.Model,
+                dto.Description
+            );
+
+            return Ok(provision);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProvision(Guid id)
+        {
+            _provisionRepository.DeleteProvision(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ ConnectorService/Models/ConnectorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConnectorService.Models
+{
+    public class ConnectorDbContext : DbContext
+    {
+        public ConnectorDbContext(DbContextOptions<ConnectorDbContext> options) : base(options) { }
+
+        public DbSet<Provision> Provisions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Provision>()
+                .ToTable("Provisions");
+
+            modelBuilder.Entity<Provision>()
+                .Property(c => c.Model)
+                .HasConversion<string>();
+        }
+    }
+}
