8ee102fa1ead667b00d03f73adbca65549192440
Stephan <57194608+stephan418@users.noreply.github.com>
2023-03-08 01:31:45 +0100

    Restructure provisions and add MassTransit

---
+++ ProvisionService/Models/ProvisionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProvisionService.Models
+{
+    public class ProvisionDbContext : DbContext
+    {
+        public ProvisionDbContext(DbContextOptions<ProvisionDbContext> options) : base(options) { }
+
+        public DbSet<Provision> Provisions { get; set; }
+    }
+}
--- ProvisionService/Program.cs
+++ ProvisionService/Program.cs
@@ -1,7 +1,8 @@
-using ConnectorService;
-using ConnectorService.Models;
+using ProvisionService.Models;
 using Microsoft.EntityFrameworkCore;
-using MQTTnet.AspNetCore;
+using MassTransit;
+using System.Reflection;
+using ProvisionService.Consumers;
 
 string? dbHostString = Environment.GetEnvironmentVariable("db_host_string");
 
@@ -10,36 +11,39 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProvisionRepository, ProvisionRepository>();
 
-builder.Services.AddDbContext<ConnectorDbContext>(options =>
+builder.Services.AddDbContext<ProvisionDbContext>(options =>
 {
     string? connectionString = dbHostString != null
-       ? dbHostString + "Database=connector;"
-       : builder.Configuration["ConnectionStrings:ConnectorDbContextConnection"];
+       ? dbHostString + "Database=provision;"
+       : builder.Configuration["ConnectionStrings:ProvisionDbContextConnection"];
 
     options.UseNpgsql(connectionString);
 });
 
-builder.Services.AddHostedMqttServer(builder =>
+builder.Services.AddMassTransit(x =>
 {
-    CustomMqttServer.BuildServerOptions(builder);
+    x.SetKebabCaseEndpointNameFormatter();
+
+    x.AddConsumer<ProvisionExistenceConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
 });
-
-builder.Services.AddMqttConnectionHandler();
-builder.Services.AddConnections();
-
-builder.WebHost.UseKestrel(k =>
-{
-    k.ListenAnyIP(1883, l => l.UseMqtt());
-
-    k.ListenAnyIP(5000);
-});
-
 
 var app = builder.Build();
 
@@ -56,18 +60,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseMqttServer(server =>
-{
-
-    server.ValidatingConnectionAsync += CustomMqttServer.BuildCustomConnectionValidator(app.Services);
-});
-
 app.Run();
 
 static void ApplyMigration(WebApplication app)
 {
     using var scope = app.Services.CreateScope();
-    var context = scope.ServiceProvider.GetRequiredService<ConnectorDbContext>();
+    var context = scope.ServiceProvider.GetRequiredService<ProvisionDbContext>();
 
     if (context.Database.GetPendingMigrations().Any())
     {
