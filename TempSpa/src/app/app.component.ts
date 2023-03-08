955858d89cf5b009007cb69ee3beec39e8f8b1e8
Laurin <60652077+Flexla54@users.noreply.github.com>
2023-03-08 08:43:46 +0100

    added basic connection between dashboard and APS

--- TempSpa/src/app/app.component.ts
+++ TempSpa/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { authCodeFlowConfig } from './auth.config';
+import { authCodeFlowConfig } from './config/auth.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'TempSpa';
--- TempSpa/src/app/dashboard/dashboard.component.ts
+++ TempSpa/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   display: boolean = false;
@@ -11,35 +11,55 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     let week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    for (let i = 0; i < (new Date).getDay() + 6; i++) {
+    for (let i = 0; i < new Date().getDay() + 6; i++) {
       week.push(week.shift()!);
     }
 
     let data = [];
-    let avg_data = []
+    let avg_data = [];
     for (let i = 0; i < 7; i++) {
       data.push(Math.floor(Math.random() * 500));
-      avg_data.push(Math.floor(Math.random() * 125) + Math.floor(Math.random() * 125) + Math.floor(Math.random() * 125) + Math.floor(Math.random() * 125));
+      avg_data.push(
+        Math.floor(Math.random() * 125) +
+          Math.floor(Math.random() * 125) +
+          Math.floor(Math.random() * 125) +
+          Math.floor(Math.random() * 125)
+      );
     }
 
     this.plugdata = {
       labels: week,
       datasets: [
         {
-            label: 'Plug Consumption last week',
-            data: data,
-            fill: false,
-            borderColor: '#42A5F5',
-            tension: 0
+          label: 'Plug Consumption last week',
+          data: data,
+          fill: false,
+          borderColor: '#42A5F5',
+          tension: 0,
         },
         {
-            label: 'avg. last 5 weeks',
-            data: avg_data,
-            fill: false,
-            borderColor: '#A542F5',
-            tension: 0.5
-        }
-      ]
-  };
+          label: 'avg. last 5 weeks',
+          data: avg_data,
+          fill: false,
+          borderColor: '#A542F5',
+          tension: 0.5,
+        },
+      ],
+    };
+  }
+
+  async addDevice() {
+    let response = await fetch('https://localhost/api/provisions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    let deviceId = response.json();
+
+    window.open('localhost/ConnectPlug?id=' + deviceId);
+
+    // TODO: Some code that fires when getting the approval that the device connected
   }
 }
