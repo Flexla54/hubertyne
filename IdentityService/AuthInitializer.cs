461325d99983febc4856ebffd465c76133728aa1
Stephan <57194608+stephan418@users.noreply.github.com>
2023-03-10 10:54:43 +0100

    Update auth configs for use in kubernetes

--- IdentityService/AuthInitializer.cs
+++ IdentityService/AuthInitializer.cs
@@ -58,7 +58,7 @@ namespace IdentityService
                 {
                     ClientId = "hubertyne-spa",
                     DisplayName = "Hubertyne Client SPA (Dashboard)",
-                    RedirectUris = { new Uri("http://localhost:4200/home"), new Uri("https://oauthdebugger.com/debug") },
+                    RedirectUris = { new Uri("http://localhost:4200/home"), new Uri("https://oauthdebugger.com/debug"), new Uri("https://dashboard.localhost/home") },
                     Type = OpenIddictConstants.ClientTypes.Public,
                     Permissions =
                     {
--- TempSpa/src/app/auth.config.ts
+++ TempSpa/src/app/auth.config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
   loginUrl: 'https://identity.localhost/connect/authorize',
-  redirectUri: 'http://localhost:4200/home',
+  redirectUri: 'https://dashboard.localhost/home',
   tokenEndpoint: 'https://identity.localhost/connect/token',
   issuer: 'https://identity.localhost/',
   clientId: 'hubertyne-spa',
