bbeb3a713bc6f81526882c74a0d055061acc2b8b
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-04-19 12:47:34 +0200

    Feature/plug page WIP to synchronize with other team members (#39)

---
+++ TempSpa/src/app/components/plugdetail/plugdetail.component.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Plug } from "../../models/plug";
+
+@Component({
+  selector: "app-plugdetail",
+  templateUrl: "./plugdetail.component.html",
+  styleUrls: ["./plugdetail.component.scss"],
+})
+export class PlugdetailComponent implements OnInit {
+  pluginformation: Plug | null = null;
+  loaded: boolean = false;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  async ngOnInit(): Promise<void> {
+    new Promise((f) => setTimeout(f, 100)).then(() => {
+      this.pluginformation = {
+        id: "1",
+        name: "living room #1",
+        addedDate: new Date("2023-03-22T07:56:38+0000"),
+        isConnected: false,
+        isTurnedOn: false,
+        userId: "admin",
+        statistics: {
+          labels: Array.from({ length: 10 }, (_, i) => i + 1),
+          datasets: [
+            {
+              label: "Plug Consumption last week",
+              data: Array.from({ length: 10 }, (_) => Math.random() * 20),
+              fill: false,
+              borderColor: "#42A5F5",
+              tension: 0,
+            },
+          ],
+        },
+      };
+      this.loaded = true;
+    });
+  }
+
+  togglePlugActive() {
+    if (this.pluginformation == null) return;
+    this.pluginformation.isTurnedOn = !this.pluginformation.isTurnedOn;
+
+    console.log("toggle plug active");
+  }
+}
--- TempSpa/src/app/config/auth.config.ts
+++ TempSpa/src/app/config/auth.config.ts
@@ -1,11 +1,11 @@
-import { AuthConfig } from 'angular-oauth2-oidc';
+import { AuthConfig } from "angular-oauth2-oidc";
 
 export const authCodeFlowConfig: AuthConfig = {
-  loginUrl: 'https://identity.localhost/connect/authorize',
-  redirectUri: 'https://dashboard.localhost/home',
-  tokenEndpoint: 'https://identity.localhost/connect/token',
-  issuer: 'https://identity.localhost/',
-  clientId: 'hubertyne-spa',
-  responseType: 'code',
-  scope: 'openid api',
+  loginUrl: "https://identity.localhost/connect/authorize",
+  redirectUri: "http://localhost:4200/home",
+  tokenEndpoint: "https://identity.localhost/connect/token",
+  issuer: "https://identity.localhost/",
+  clientId: "hubertyne-spa",
+  responseType: "code",
+  scope: "openid api",
 };
