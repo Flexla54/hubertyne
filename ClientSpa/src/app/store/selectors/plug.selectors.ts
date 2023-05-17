6594fb7c2f45a530d4f865ae6a2fc4ec7b344fd8
Laurin <60652077+Flexla54@users.noreply.github.com>
2023-05-17 12:36:26 +0200

    added plug selector

--- ClientSpa/src/app/store/selectors/plug.selectors.ts
+++ ClientSpa/src/app/store/selectors/plug.selectors.ts
@@ -1,3 +1,12 @@
+import { createSelector } from '@ngrx/store';
 import { AppState } from '../state/app.state';
 
 export const selectPlugs = (state: AppState) => state.plugs;
+
+export const selectSpecificPlug = (id: string) =>
+  createSelector(selectPlugs, (plugs) => {
+    plugs = plugs.filter((plug) => {
+      return plug.id == id;
+    });
+    return plugs.slice();
+  });
