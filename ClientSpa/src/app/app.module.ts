53cf99691c2511879144036574c5cd137b57d3ea
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-05-31 10:34:13 +0200

    plug overview on homepage; frontend only, backend missing :( (#45)
    * styling changes
    
    * multi plug view analysis

--- ClientSpa/src/app/app.module.ts
+++ ClientSpa/src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { OAuthModule } from "angular-oauth2-oidc";
+import { OAuthModule } from 'angular-oauth2-oidc';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-import { AvatarModule } from "primeng/avatar";
-import { ButtonModule } from "primeng/button";
-import { ChartModule } from "primeng/chart";
-import { ProgressSpinnerModule } from "primeng/progressspinner";
-import { SidebarModule } from "primeng/sidebar";
-import { SkeletonModule } from "primeng/skeleton";
-import { ToggleButtonModule } from "primeng/togglebutton";
-import { ToolbarModule } from "primeng/toolbar";
-import { PlugdetailComponent } from "./components/plugdetail/plugdetail.component";
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { SidebarModule } from 'primeng/sidebar';
+import { SkeletonModule } from 'primeng/skeleton';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { PlugdetailComponent } from './components/plugdetail/plugdetail.component';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, PlugdetailComponent],
@@ -28,7 +32,11 @@ import { PlugdetailComponent } from "./components/plugdetail/plugdetail.componen
     OAuthModule.forRoot(),
     AvatarModule,
     ButtonModule,
+    CalendarModule,
+    CardModule,
     ChartModule,
+    DropdownModule,
+    FormsModule,
     HttpClientModule,
     ProgressSpinnerModule,
     SidebarModule,
