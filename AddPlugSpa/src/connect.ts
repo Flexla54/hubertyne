dc3712f4d1d52b8231f82d2d6a4f9b84d6e1d223
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-06-07 11:46:36 +0200

    hopefully fixed redirection bug

--- AddPlugSpa/src/connect.ts
+++ AddPlugSpa/src/connect.ts
@@ -3,6 +3,7 @@ async function onSubmit(e: Event) {
 
   const searchParams = new URLSearchParams(window.location.search);
   const deviceId = searchParams.get('id');
+  const mqtt_ip = 'mqtt.hubertyne.me';
 
   let ssid = (document.getElementById('ssid') as HTMLInputElement).value;
   let pass = (document.getElementById('pw') as HTMLInputElement).value;
@@ -10,9 +11,9 @@ async function onSubmit(e: Event) {
   await fetch('http://192.168.33.1/settings?allow_cross_origin=1').catch(); // Enable CORS
   await fetch(
     `http://192.168.33.1/settings?mqtt_enable=true
-      &mqtt_user=` +
-      { deviceId } +
-      `&mqtt_pass=password&mqtt_server=192.168.227.65:1883`
+      &mqtt_user=${deviceId}
+      &mqtt_pass=password
+      &mqtt_server=${mqtt_ip}`
   ); // Enable and configure MQTT
   await fetch(`http://192.168.33.1/settings?coiot_enable=0`); // Disable CoIot
 
@@ -38,3 +39,19 @@ function togglePassword() {
     x.type = 'password';
   }
 }
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    console.log('hello');
+    let home = document.getElementById('homeLink');
+    if (home) {
+      home.setAttribute(
+        'href',
+        new URLSearchParams(window.location.search).get('success-redirect') ||
+          'https://www.hubertyne.me/home'
+      );
+    }
+  },
+  false
+);
