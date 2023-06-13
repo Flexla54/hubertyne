2716b2934bdf602f1be5e3587319ca58652b2550
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-06-13 16:55:07 +0200

    with my previous "fix" I did nothing and now I revert that

--- ClientSpa/src/environments/environment.development.ts
+++ ClientSpa/src/environments/environment.development.ts
@@ -1,9 +1,5 @@
 export const environment = {
-<<<<<<< HEAD
   dashboardHost: 'localhost:4200',
   identityHost: "identity.localhost",
   addPlugHost: "addplug.localhost"
-=======
-  domain: 'api.hubertyne.me',
->>>>>>> 0c40188 (changed address from local to production)
 };
