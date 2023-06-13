c0b83efb154763f5adc0def46a0b8d1fbbeb76e8
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2023-06-13 16:41:13 +0200

    hopefully didn't break everything oops

--- ClientSpa/src/environments/environment.development.ts
+++ ClientSpa/src/environments/environment.development.ts
@@ -1,5 +1,9 @@
 export const environment = {
+<<<<<<< HEAD
   dashboardHost: 'localhost:4200',
   identityHost: "identity.localhost",
   addPlugHost: "addplug.localhost"
+=======
+  domain: 'api.hubertyne.me',
+>>>>>>> 0c40188 (changed address from local to production)
 };
